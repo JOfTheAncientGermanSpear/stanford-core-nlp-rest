7e324f8b31124f26b629edb2387ea15a832125d3
bonihlalab <bonihlalab@bonihlalab.local>
2017-09-10 12:30:21 -0400

    more robust url encoding

--- demo-client-webapp/nlp_client.py
+++ demo-client-webapp/nlp_client.py
@@ -1,12 +1,14 @@
 import requests
+import urllib
 
 base_url = 'http://localhost:9000/api/parse/'
+
 
 def sanitize(text):
 	return text.replace('/', '%2f')
 
 
 def parse(text):
-	url = base_url + sanitize(text.strip())
+	url = base_url + urllib.quote(sanitize(text.strip()))
 	response = requests.get(url)
 	return response.json()
