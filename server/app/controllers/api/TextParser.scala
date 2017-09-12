37ef5a30a258da715f6bf247799d2dd799cb5eea
bonihlalab <bonihlalab@bonihlalab.local>
2017-09-12 12:12:33 -0400

    add ability to accept post data

--- server/app/controllers/api/TextParser.scala
+++ server/app/controllers/api/TextParser.scala
@@ -1,15 +1,33 @@
 package controllers.api
 
 import edu.stanford.nlp.pipeline.Annotation
+import play.api.libs.json._
 import play.api.mvc._
 
 
 object TextParser extends Controller with utils.TextParserJson {
 
-  def parse_text_action(text: String) = Action {
+  def parse_to_json(text:String) = {
     val doc:Annotation = parse_text(text)
-    val json = convert_document(doc)
-    Ok(json)
+    convert_document(doc)
+  }
+
+  def parse_text_action(text: String) = Action {
+    Ok(parse_to_json(text))
+  }
+
+  def parse_body_action = Action { request: Request[AnyContent] =>
+    val body: AnyContent = request.body
+    val data = body.asFormUrlEncoded.get
+
+    data.get("text") match {
+      case Some(text_seq) =>  
+        if (text_seq.length == 1) 
+          Ok(parse_to_json(text_seq.head)) else 
+          BadRequest("text field must be length 1")
+      case None => BadRequest(
+        "Expecting request body with text field")
+    }
   }
 
 }
